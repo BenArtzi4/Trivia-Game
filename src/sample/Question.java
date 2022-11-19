a64993414157c49d27bc6971cd3d1ba06fff5e41
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2022-11-19 21:23:58 +0200

    add try and catch to read file text

--- src/sample/Question.java
+++ src/sample/Question.java
@@ -2,6 +2,7 @@ package sample;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 /*
@@ -66,40 +67,42 @@ public class Question
      */
     private void readQuestion(int questionNUmber) throws FileNotFoundException {
         int lineCounter = 0;
-        Scanner input = new Scanner(new File("trivia.txt"));
-        // Skipping over all irrelevant lines
-        while (lineCounter < (questionNUmber*ROWS_IN_QUESTION))
-        {
-            String st = input.nextLine();
-            lineCounter++;
-        }
-        lineCounter = 0;
-        while (input.hasNext() && lineCounter < ROWS_IN_QUESTION )
-        {
-            String st = input.nextLine();
-            switch (lineCounter)
-            {
-                case 0:
-                    question = st;
-                    break;
-                case 1:
-                    rightAnswer = st;
-
-                    break;
-                case 2:
-                    wrongAnswer1 = st;
-                    break;
-                case 3:
-                    wrongAnswer2 = st;
-
-                    break;
-                case 4:
-                    wrongAnswer3 = st;
-                    break;
+        try {
+            Scanner input = new Scanner(new File("trivia.txt"));
+            // Skipping over all irrelevant lines
+            while (lineCounter < (questionNUmber * ROWS_IN_QUESTION)) {
+                String st = input.nextLine();
+                lineCounter++;
             }
-            lineCounter++;
+            lineCounter = 0;
+            while (input.hasNext() && lineCounter < ROWS_IN_QUESTION) {
+                String st = input.nextLine();
+                switch (lineCounter) {
+                    case 0:
+                        question = st;
+                        break;
+                    case 1:
+                        rightAnswer = st;
+
+                        break;
+                    case 2:
+                        wrongAnswer1 = st;
+                        break;
+                    case 3:
+                        wrongAnswer2 = st;
+
+                        break;
+                    case 4:
+                        wrongAnswer3 = st;
+                        break;
+                }
+                lineCounter++;
+            }
+            input.close();
+        } catch (IOException e)
+        {
+            System.out.println("Text file not found");
         }
-        input.close();
 
 
     }
